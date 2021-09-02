5bc68fc5adb510de7624cefcf24c0ddb3f2f4ece
vishal <mangtani.vishal123@gmail.com>
2021-09-02 10:43:56 +0530

    installed styled components and created styles for search cards

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ShowCard from './ShowCard';
 import IMAGE_NOT_FOUND from "../../images/not-found.png"
+import  {FlexGrid} from "../styled";
 
 const ShowGrid = ({ data }) => {
 
-  return <div>
+  return <FlexGrid>
     {
       data.map(({show}) => <ShowCard key={show.id} name={show.name}
       image = {show.image ? show.image.medium : IMAGE_NOT_FOUND}
       summary = {show.summary}
       />)
     }
-  </div>
+  </FlexGrid>
 }
 
 export default ShowGrid;
